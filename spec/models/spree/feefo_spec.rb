58ec08693f9cf8faa1a24ef56d0714247f9dae8c
Christopher Reeve <cjreeve@gmail.com>
2016-03-08 18:41:40 +0000

    tidy up feed test

--- spec/models/spree/feefo_spec.rb
+++ spec/models/spree/feefo_spec.rb
@@ -51,38 +51,37 @@ RSpec.describe Spree::Feefo, type: :model do
       shipment = Spree::Shipment.last
       puts "#{ @feed_content[0] }"
       puts "#{ @feed_content[1] }"
-      res = expect(@feed_content).to eq(
-        res =[
-          [
-            "name",
-            "email",
-            "date",
-            "description",
-            "merchant identifier",
-            "category",
-            "feedback fate",
-            "product search code",
-            "order Ref",
-            "product link",
-            "customer Ref",
-            "amount"
-          ],
-          [
-            shipment.order.name,
-            shipment.order.email,
-            shipment.shipped_at.strftime('%Y-%m-%d'),
-            shipment.manifest.first.line_item.product.name.strip,
-            Spree::Config[:feefo_merchant_identifier],
-            shipment.manifest.first.line_item.product.taxons.order(:lft).first.try(:name).to_s,
-            '',
-            shipment.manifest.first.line_item.product.sku,
-            shipment.order.number,
-            Spree::Core::Engine.routes.url_helpers.product_url(shipment.manifest.first.line_item.product, host: Spree::Store.default.url),
-            shipment.order.user ? shipment.order.user.id : '',
-            shipment.order.total.to_f.to_s
-          ]
+      expected_result =[
+        [
+          "name",
+          "email",
+          "date",
+          "description",
+          "merchant identifier",
+          "category",
+          "feedback fate",
+          "product search code",
+          "order Ref",
+          "product link",
+          "customer Ref",
+          "amount"
+        ],
+        [
+          shipment.order.name,
+          shipment.order.email,
+          shipment.shipped_at.strftime('%Y-%m-%d'),
+          shipment.manifest.first.line_item.product.name.strip,
+          Spree::Config[:feefo_merchant_identifier],
+          shipment.manifest.first.line_item.product.taxons.order(:lft).first.try(:name).to_s,
+          '',
+          shipment.manifest.first.line_item.product.sku,
+          shipment.order.number,
+          Spree::Core::Engine.routes.url_helpers.product_url(shipment.manifest.first.line_item.product, host: Spree::Store.default.url),
+          shipment.order.user ? shipment.order.user.id : '',
+          shipment.order.total.to_f.to_s
         ]
-      )
+      ]
+      expect(@feed_content).to eq(expected_result)
     end
   end
 end
