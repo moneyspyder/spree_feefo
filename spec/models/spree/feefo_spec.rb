ad782a64c192f7b0902846890810ccfa0c1c7fe8
Christopher Reeve <cjreeve@gmail.com>
2016-03-08 18:29:15 +0000

    improve feed test

--- spec/models/spree/feefo_spec.rb
+++ spec/models/spree/feefo_spec.rb
@@ -4,7 +4,7 @@ require 'csv'
 RSpec.describe Spree::Feefo, type: :model do
 
   before(:each) do
-    Spree::Config[:feefo_merchant_identifier] = 'www.example_logon.com'
+    Spree::Config[:feefo_merchant_identifier] = 'example_site'
     Spree::Config[:feefo_feed_name] = 'test_feefo_feed.csv'
     Spree::Config[:feefo_tmp_feed_location] = 'tmp'
     Spree::Config[:feefo_public_feed_location] = ''
@@ -18,13 +18,71 @@ RSpec.describe Spree::Feefo, type: :model do
   end
 
   context "there is 1 shipped order" do
+
+    let!(:store) { FactoryGirl.create(:store, default: true ) }
+    let(:billing_address) {FactoryGirl.create(:address) }
+    let(:order) do
+      o = Spree::Order.create(email: "email@domain.com")
+      o.billing_address = billing_address
+      o.save
+      o
+    end
+    let(:variant) { create(:variant) }
+    let!(:line_item) { order.contents.add variant }
+    let!(:shipment) do
+      order.create_proposed_shipments.first
+      s = order.shipments.first
+      s.state = "shipped"
+      s.shipped_at = (DateTime.now - 48.hours)
+      s.save
+      s
+    end
+
     before(:each) do
-      FactoryGirl.create(:shipment, state: 'shipped', shipped_at: (DateTime.now - 48.hours))
+      Feefo.generate_feed
+      @feed_content = CSV.open(File.join(Rails.root, 'public/test_feefo_feed.csv'), "r", {col_sep: "\t", encoding: "UTF-8"}).read
     end
 
     it "finds 1 shipped order" do
-      Feefo.generate_feed
-      CSV.read(File.join(Rails.root, 'public/test_feefo_feed.csv')).count == 1
+      expect( @feed_content.length ).to eq(2)
+    end
+
+    it "has the expected attributes" do
+      shipment = Spree::Shipment.last
+      puts "#{ @feed_content[0] }"
+      puts "#{ @feed_content[1] }"
+      res = expect(@feed_content).to eq(
+        res =[
+          [
+            "name",
+            "email",
+            "date",
+            "description",
+            "merchant identifier",
+            "category",
+            "feedback fate",
+            "product search code",
+            "order Ref",
+            "product link",
+            "customer Ref",
+            "amount"
+          ],
+          [
+            shipment.order.name,
+            shipment.order.email,
+            shipment.shipped_at.strftime('%Y-%m-%d'),
+            shipment.manifest.first.line_item.product.name.strip,
+            Spree::Config[:feefo_merchant_identifier],
+            shipment.manifest.first.line_item.product.taxons.order(:lft).first.try(:name).to_s,
+            '',
+            shipment.manifest.first.line_item.product.sku,
+            shipment.order.number,
+            Spree::Core::Engine.routes.url_helpers.product_url(shipment.manifest.first.line_item.product, host: Spree::Store.default.url),
+            shipment.order.user ? shipment.order.user.id : '',
+            shipment.order.total.to_f.to_s
+          ]
+        ]
+      )
     end
   end
 end
